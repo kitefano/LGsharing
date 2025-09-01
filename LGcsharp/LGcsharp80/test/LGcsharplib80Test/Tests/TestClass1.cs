b13fd1a77d28d0a24eddfdd32236f311cd92e2f1
kitefano <kitefano@163.com>
2025-09-01 19:11:33 +0800

    20250901-191100

---
+++ LGcsharp/LGcsharp80/test/LGcsharplib80Test/Tests/TestClass1.cs
@@ -0,0 +1,16 @@
+﻿using LGcsharplib80;
+using LGcsharplib80.LGcommons;
+
+namespace LGcsharplib80Test.Tests
+{
+    [TestClass]
+    public class TestClass1
+    {
+        [TestMethod]
+        public void func()
+        {
+            LGsharplib80Class1.funcTest($"{LGcom.GetCallerInfo()}: a={0}, b={1}", 10, "20");
+            // LGsharplib80Class1.logTest();
+        }
+    }
+}
